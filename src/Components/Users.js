7210acda0a0701db145b5d77d40e652f64a4ef9d
Ndene Diallo <ndene.diallo@hgs.hiddengeniusproject.org>
2021-01-13 22:00:03 -0800

    new issue [1]

---
+++ src/Components/Users.js
@@ -0,0 +1,38 @@
+export const dummyUsers = [
+    {
+        id: "2",
+        name: "Kelvin Samson",
+        email: "kelvin@sample.com",
+        photoUrl: "https://picsum.photos/201",
+        role: "Member",
+        info: "Product Designer at Facebook",
+        welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    {
+        id: "3",
+        name: "Annabel Stone",
+        email: "annie@sample.com",
+        photoUrl: "https://picsum.photos/202",
+        role: "Member",
+        info: "Digital Marketer at Microsoft",
+        welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    {
+        id: "4",
+        name: "Grace Loveday",
+        email: "grace@sample.com",
+        photoUrl: "https://picsum.photos/203",
+        role: "Member",
+        info: "Product Designer at Google",
+        welcomeMessage: "Hey there! Love to chat :-)"
+    },
+    {
+        id: "5",
+        name: "Angela Jobs",
+        email: "angela@sample.com",
+        photoUrl: "https://picsum.photos/204",
+        role: "Member",
+        info: "Software Engineer at TalkJS",
+        welcomeMessage: "Hey there! Love to chat :-)"
+    },
+]
